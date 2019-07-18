6a1b1473fc141e721c8ab6c843d9e8b75005e883
Gabriele Lanzafame <gabrylanz@gmail.com>
2019-07-18 18:27:52 +0200

    Move function List in list.ts

--- src/main.ts
+++ src/main.ts
@@ -3,65 +3,9 @@
 // All of the Node.js APIs are available in this process.
 
 import * as $ from 'jquery';
+import { ItemList, loadList, setItem, getList, deleteItem, deleteList } from './list';
 
-interface ItemList {
-  id: number,
-  title: string,
-  text: string
-};
-
-const getList = (): Array<ItemList> => {
-  if (localStorage.getItem('data') !== 'undefined') {
-    return JSON.parse(localStorage.getItem('data'));
-  } else {
-    localStorage.setItem('data', JSON.stringify([]));
-    return JSON.parse(localStorage.getItem('data'));
-  }
-};
-
-const setItem = (obj: ItemList): boolean => {
-  let data: Array<ItemList> = getList();
-
-  if (data.length != 1000) {
-    if (obj.title === '' || obj.text === '') {
-      alert('Title or text is empty!');
-      return false;
-    }
-
-    data.push(obj);
-    localStorage.setItem('data', JSON.stringify(data));
-
-    alert('List: ' + data.length +'/1000')
-
-    return true;
-  }
-
-  return false;
-};
-
-const loadList = (): void => {
-  let data: Array<ItemList> = getList();
-  for (let i = 0; i < data.length; i++)
-    if (data[i].title == 'null') {
-      console.log('NULL');
-    } else {
-      $('#list').append('<li id="' + data[i].id + '" class="list-item clearfix"><div class="row"><div class="col-9"><h2>' + data[i].title + '</h2><p>' + data[i].text + '</p></div><div class="col-3"><button id="delete-item" type="button" value="Delete" data-id="' + data[i].id +'"><i class="material-icons">clear</i></button></div></li>');
-    }
-};
-
-const deleteItem = (id: number): void => {
-  let data: Array<ItemList> = getList();
-  data[id].title = 'null';
-  localStorage.setItem('data', JSON.stringify(data));
-  location.reload();
-}
-
-const deleteList = (): void => {
-  localStorage.removeItem('data');
-  location.reload();
-}
-
-const main = (): void => {
+$(window).ready((): void => {
   if (localStorage.getItem('data') === null) {
     localStorage.setItem('data', JSON.stringify([]));
   } else {
@@ -74,7 +18,15 @@ const main = (): void => {
     let text: string = <string>$('#text').val();
 
     if (data.length == 0) {
-      var item: ItemList = { id: 0, title: title, text: text };
+      let d: string = new Date().toString();
+      let date: string;
+
+      if (d.includes('GMT+0200 (Ora legale dell’Europa centrale)')) {
+        date = d.toString().replace('GMT+0200 (Ora legale dell’Europa centrale)', '');
+      }
+
+      title = title.replace(/\n/, '<br />');
+      let item: ItemList = { id: 0, title: title, text: text, dateToCreation: date.toString() };
 
       if (setItem(item)) {
         location.reload();
@@ -82,7 +34,15 @@ const main = (): void => {
         alert('Error to add item in the list');
       }
     } else {
-      var item: ItemList = { id: data[data.length - 1].id + 1, title: title, text: text };
+      let d: string = new Date().toString();
+      let date: string;
+
+      if (d.toString().includes('GMT+0200 (Ora legale dell’Europa centrale)')) {
+        date = d.toString().replace('GMT+0200 (Ora legale dell’Europa centrale)', '');
+      }
+
+      title = title.replace(/\r?\n/g, '<br />');
+      let item: ItemList = { id: data[data.length - 1].id + 1, title: title, text: text, dateToCreation: date.toString() };
 
       if (setItem(item)) {
         location.reload();
@@ -92,14 +52,10 @@ const main = (): void => {
     }
   });
 
-  $('#delete-item').click(function(e: JQuery.Event): void {
+  $('#delete-item').click(function (e: JQuery.Event): void {
     let index: JQuery.Selector = $(this).attr('data-id');
     deleteItem(parseInt(index));
   });
 
   $('#delete-list').click((e: JQuery.Event): void => { deleteList(); });
-};
-
-$(window).ready((): void => {
-  main();
 });
